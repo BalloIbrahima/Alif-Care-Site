98eeb2fddfb2a310bc1a28915c92956ccbaa9dc4
Ballo <ibrahimaballo01@gmail.com>
2022-04-01 10:32:36 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Alif-Care-Site" 
---
+++ index.php
@@ -0,0 +1,502 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="icon"   href="assets/images/icon_site2.ico">
+    <title>Alif Care</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!--
+
+TemplateMo 570 Chain App Dev
+
+https://templatemo.com/tm-570-chain-app-dev
+
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a class="logo_big logo" style="display: flex;width:max-content;" >
+              <img  src="assets/images/9.png" height="100" width="auto" alt="Alif Care">
+            </a>
+
+            <a href="index.html" class="logo logo_mini" style="display: none;">
+              <img src="assets/images/logo.png" alt="Chain App Dev">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Acceuil</a></li>
+              <li class="scroll-to-section"><a href="#services">Nos services</a></li>
+              <li class="scroll-to-section"><a href="#about">A Propos</a></li>
+              <li class="scroll-to-section"><a href="#souscription">Comment souscrire ?</a></li>
+              <li class="scroll-to-section"><a href="#newsletter">Nous contacter</a></li>
+              <li><div style="display: none;"  class="gradient-button"><a id="modal_trigger" href="#modal"><i class="fa fa-sign-in-alt"></i> Sign In Now</a></div></li>
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+  
+  <div id="modal" class="popupContainer" style="display:none;">
+    <div class="popupHeader">
+        <span class="header_title">Login</span>
+        <span class="modal_close"><i class="fa fa-times"></i></span>
+    </div>
+
+    <section class="popupBody">
+        <!-- Social Login -->
+        <div class="social_login">
+            <div class="">
+                <a href="#" class="social_box fb">
+                    <span class="icon"><i class="fab fa-facebook"></i></span>
+                    <span class="icon_title">Connect with Facebook</span>
+
+                </a>
+
+                <a href="#" class="social_box google">
+                    <span class="icon"><i class="fab fa-google-plus"></i></span>
+                    <span class="icon_title">Connect with Google</span>
+                </a>
+            </div>
+
+            <div class="centeredText">
+                <span>Or use your Email address</span>
+            </div>
+
+            <div class="action_btns">
+                <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
+                <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
+            </div>
+        </div>
+
+        <!-- Username & Password Login form -->
+        <div class="user_login">
+            <form>
+                <label>Email / Username</label>
+                <input type="text" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="remember" type="checkbox" />
+                    <label for="remember">Remember me on this computer</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Login</a></div>
+                </div>
+            </form>
+
+            <a href="#" class="forgot_password">Forgot password?</a>
+        </div>
+
+        <!-- Register Form -->
+        <div class="user_register">
+            <form>
+                <label>Full Name</label>
+                <input type="text" />
+                <br />
+
+                <label>Email Address</label>
+                <input type="email" />
+                <br />
+
+                <label>Password</label>
+                <input type="password" />
+                <br />
+
+                <div class="checkbox">
+                    <input id="send_updates" type="checkbox" />
+                    <label for="send_updates">Send me occasional email updates</label>
+                </div>
+
+                <div class="action_btns">
+                    <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
+                    <div class="one_half last"><a href="#" class="btn btn_red">Register</a></div>
+                </div>
+            </form>
+        </div>
+    </section>
+</div>
+
+  <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-6 align-self-center">
+              <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <h2>Retrouvez notre application sur les stores </h2>
+                    <p>Android sur PLAYSTORE et iOS sur APPSTORE.</p>
+                  </div>
+                  <div class="col-lg-12">
+                    <div class="white-button first-button scroll-to-section">
+                      <a href="#contact">Android<i class="fab fa-google-play"></i></a>
+                    </div>
+                    <div class="white-button scroll-to-section">
+                      <a href="#contact">iOS<i class="fab fa-apple"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                <img src="assets/images/slider-dec.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="services" class="services section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+            <h4>Chez <em>Alif Care</em> nous vous offrons différents services</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Avec notre application Alif care vous pouvez souscrire à votre assurance préféré en seulement quelques clics et en plus sans bouger.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="first-service">
+            <div class="icon"></div>
+            <h4>Assurance Auto</h4>
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="second-service">
+            <div class="icon"></div>
+            <h4>Assurance Moto</h4>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="third-service">
+            <div class="icon"></div>
+            <h4>Assurance voyage</h4>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="fourth-service">
+            <div class="icon"></div>
+            <h4>Assurance Sante</h4>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="six-service">
+            <div class="icon"></div>
+            <h4>Assurance Construction</h4>
+            
+          </div>
+        </div>
+        <!-- <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="seven-service">
+            <div class="icon"></div>
+            <h4>Assurance Habitation</h4>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="five-service">
+            <div class="icon"></div>
+            <h4>Assurance Multirisque</h4>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 list_assurance" style="margin-bottom: 1%; display: flex;justify-content: center;">
+          <div class="eigt-service">
+            <div class="icon"></div>
+            <h4>Autres Assurances</h4>
+           
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </div>
+
+  <div id="about" class="about-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading">
+            <h4>Qu'est ce que<em> nous </em>faisons & qui sommes nous?</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>
+            </p>
+          </div>
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="box-item">
+                <h4><a href="#">Notre rôle!</a></h4>
+                <p>Avec les conseilles de nos experts en assurance nous vous
+                aidons à acheter votre assurance au près de votre assureur préferé très rapidement et surement.
+                Nos experts en assurance vous propose l'assurance qu'il vous faut au prix qu'il vous faut.</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="box-item">
+                <h4><a href="#">Qui sommes nous?</a></h4>
+                <p>Alif Care est votre courtier d'assurance par excelence.</p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-image">
+            <img src="assets/images/about-right-dec.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+  <div id="souscription" class="pricing-tables">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+            <div class="section-heading">
+            <h4>Comment souscrire à votre <em>assurance</em></h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Avec notre application mobile Alif Care souscrire à votre assurance est très simple!</p>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <span class="price">à 0Fcfa</span>
+            <h4>Télécharger et installer l'application Mobile</h4>
+            <div class="icon">
+              <img src="assets/images/icon_site2.png" alt="">
+            </div>
+            <ul>
+              <li>Alif Care</li>
+             
+              <li>
+                  <div class="border-button">
+                    <a href="#">Android sur PLAYSTORE</a>
+                  </div>
+              </li>
+              <li>
+                  <div class="border-button">
+                    <a href="#">iOS sur APPSTORE</a>
+                  </div>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <h4>Vérifier votre numéro de téléphone </h4>
+            <div>
+                <img src="assets/images/accteuil.PNG" style="height: 300px;width: auto;" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <span class="price"></span>
+            <h4>Clicquer sur Souscrire à une assurance</h4>
+            <div class="icon">
+                <img src="assets/images/home.PNG" style="height: 300px;width: auto;" alt="">
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-3">
+          <div class="pricing-item-regular">
+            <h4>Uphloader et valider le document démandé</h4>
+            <div>
+                <img src="assets/images/afterchoice.PNG" style="height: 300px;width: auto;" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+
+  <footer id="newsletter">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading">
+            <h4>Pour vos besoins en assurance, Contactez Alif Care</h4>
+          </div>
+        </div>
+        <!-- <div class="col-lg-6 offset-lg-3">
+          <form id="search" action="#" method="GET">
+            <div class="row">
+              <div class="col-lg-6 col-sm-6">
+                <fieldset>
+                  <input type="address" name="address" class="email" placeholder="Email Address..." autocomplete="on" required>
+                </fieldset>
+              </div>
+              <div class="col-lg-6 col-sm-6">
+                <fieldset>
+                  <button type="submit" class="main-button">Subscribe Now <i class="fa fa-angle-right"></i></button>
+                </fieldset>
+              </div>
+            </div>
+          </form>
+        </div> -->
+      </div>
+
+
+
+      <div class="row scaind_row_foot">
+        <div class="adresse_footer col-lg-3">
+          <div class="footer-widget adresse_foot">
+            <h4>Adresses</h4>
+            <p>Hamdalaye ACI 2000 rue 360 Imm. MAMADOU SOW</p>
+            <p ><a>+223 71806167</a></p>
+            <p><a href="mailto::alifdigital8@gmail.com?subject=AlifCareProtect">info@alifcare.site</a></p>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="footer-widget">
+            <h4>Reseaux sociaux</h4>
+            <ul>
+              <li><a href="https://wa.me/22377786028?text=Salut%20" target="_blank">WhatsApp</a></li>
+              <li><a href="https://www.facebook.com/102506305659766/posts/102509778992752/?substory_index=0" target="_blank">Facebook</a></li>
+              <li><a href="#">Linkedin</a></li>
+              <!--<li><a href="#">Testimonials</a></li>
+              <li><a href="#">Pricing</a></li>-->
+            </ul>
+
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="footer-widget">
+            <h4>Nos autres filiales</h4>
+            <ul>
+              <li><a href="https://alifcorp.net" target="_blank">Alif Corp</a></li>
+              <li><a href="https://blebleba.com" target="_blank">Blebleba</a></li>
+              <li><a href="https://alifcorp.net" target="_blank">Chap Chap Tech</a></li>
+              <li><a href="https://kashbooster.com" target="_blank">Kash Booster</a></li>
+              <li><a href="https://soutraso.com" target="_blank">Soutraso</a></li>
+            </ul>
+            <ul>
+              <li><a href="https://minafika.com" target="_blank">Minafika</a></li>
+              <li><a href="https://alifshop.com" target="_blank">Alif Shop</a></li>
+              <li><a href="#" target="_blank">autres</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="footer-widget">
+            <h4>A propos de Alif Corp</h4>
+            <div class="logo">
+                <img src="assets/images/alif.png"  alt="Alif Corp">
+            </div>
+            <p>Le Groupe Alif Corp est une entreprise s'activant dans 3 secteurs clé :</p>
+
+
+                <p>- La finance Digitale & la monétique à travers Alif Financial Services</p>    
+
+                <p>- le consulting et la formation à travers Alif business Booster</p>    
+
+                <p>- le E-business à  travers notre  Alif Shop </p>    
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="copyright-text">
+            <p>Copyright © 2022 Alif Corp. 
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!--scroll btn-->
+  <div class="scroll_btn" style="color:#0c4128; font-size: 40; cursor: pointer; position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 1000000;">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+     </svg>
+  </div>
+  <!--end scroll btn-->
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+  <script>
+    const scroll = document.querySelector(".scroll_btn");
+    scroll.onclick=function(){
+            window.scrollTo({
+                top:0,
+                left:0,
+                behavior:'smooth',
+            })
+        }
+      </script>
+</body>
+</html>
