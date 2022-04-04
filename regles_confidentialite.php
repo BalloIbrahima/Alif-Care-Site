681bab0c4e101a09893274ba845744ae4d212025
Ballo <ibrahimaballo01@gmail.com>
2022-04-04 12:26:04 +0000

    04 04 2022 1226

---
+++ regles_confidentialite.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="icon"   href="assets/images/icon_site2.ico">
+    <title>Alif Care | Règles de Confidentialité</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/templatemo-chain-app-dev.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/regle.css">
+
+  </head>
+
+    <body>
+
+    <h1 class="importante_phrase"><strong>Alif Care</strong>:Règles de Confidentialité</h1>
+        <!-- <h1>Notre site a été conçu dans un seul but :</h1>
+        <p>Vulgarisez auprès de la population malienne et de diaspora les résultats des centres et institutions de recherches.</p> -->
+        <div>
+            <p class="question question1">Règles de Confidentialité</p>
+            <div class="reponse1">
+                <p> 
+                    Alif Care est une application mobile de e-assurance appartenant à <strong>ALIF CORP</strong> est une société Malienne.
+            
+                </p>
+                
+            
+                <p>
+                 Alif Corp est situé à BAMAKO, HADALAYE ACI 2000 près de "Bougie ba".                </p>
+            </div>
+        </div>
+
+        <div>
+            <p class="question2 question">Données personnelles</p>
+            <div class="reponse2">
+                <p>
+                    Avec votre consentement, depuis notre Application, nous  collectons quelques une de vous données personnelle dresses de courrier électronique ou les adresses IP complètes).        
+                </p>
+                <p>
+                    Votre numéro de téléphone: pour pouvoir vous authentifiez en premier lieu, vous permettre d'accéder à l'application et enfin vous contactez pour vous fornir votre demande.
+                </p>
+                <p>
+                 Votre nom complet: qui vas nous permettre de vous identifiez pour vous fournir un meuilleur service le plus rapidement possible.
+                </p>
+                <p>
+                 Votre adresse : qui est receulli pour votre fiche d'assurance, et qui qui vas aussi nous permettre de vous faire une livraison si possible.
+                </p>
+                <p>
+                 Votre adresse electronique(adresse email) : qui est une autre manière de vous contactez et/ou de vous authentifiez.
+                </p>
+                <p>
+                Le type d’appareil utilisé,
+                </p>
+                <p>
+                   D'autres informations peuvent t'être receulli mais avec votre consentement et pas votre issue.        
+                </p>
+            </div>
+        </div>
+
+        <!-- <div>
+            <p class="question3 question">Nos différents canaux</p>
+            <div class="reponse3">
+                <ul>
+                    <li>
+                        Site web Internet;
+                    </li>
+                    <li>
+                        Télévision;
+                    </li>
+                    <li>
+                        Radio;
+                    </li>
+                    <li>
+                        Magazines;
+                    </li>
+                    <li>
+                        Newspapers
+                    </li>
+                </ul>
+                
+            </div>
+
+
+        </div> -->
+
+        
+        <div>
+            <p class="question5 question"></p>
+        </div>
+        <div>
+            <p class="question6 question">Permissions demandées par l’Application</p>
+            <div class="reponse6">
+                <li class="sous_question">Utilisation de la caméra</li>
+                <p>Nous utilisons uniquement l’appareil photo pour prendre en photo soit votre <strong>Carte grise</strong> si c'est une assurance auto, soit votre <strong>Vignette</strong> si c'est une assurance moto et ainsi de suite.Cela vas dépandre du type d'assurance à la quelle vous allez souscrire.
+                </p>
+
+                <li class="sous_question">Accès aux documents du téléphone</li>
+                <p>Nous utilisons l'accès aux documents du telephone a fin de permettre au client de pouvoir Uphloader le type de document démandé.              Il est en charge d'optimiser les chiffres d'affaires et de participer au développement des activités du journal.
+                </p>
+
+                
+        </div>
+        <div class="col-lg-12">
+          <div class="copyright-text">
+            <p>Copyright © 2022 Alif Corp. 
+          </div>
+        </div>
+    </body>
+</html>
